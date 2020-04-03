9bbfb1ecbb09b826ff28753a88293640f9e6ae75
Lauren Moor <laurenkaede@gmail.com>
2020-04-03 16:02:49 +0100

    Updates

---
+++ precache-manifest.bf151a94dd776a6ca29b8ca5017fd063.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c467d3cecaf1cfca9ad65650b3ec73",
+    "url": "/React-Address-Book/index.html"
+  },
+  {
+    "revision": "3e004e9651687ff50e5c",
+    "url": "/React-Address-Book/static/css/main.3b9b05a1.chunk.css"
+  },
+  {
+    "revision": "7b9274865b650801e621",
+    "url": "/React-Address-Book/static/js/2.52efe9fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Address-Book/static/js/2.52efe9fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e004e9651687ff50e5c",
+    "url": "/React-Address-Book/static/js/main.c53c17fe.chunk.js"
+  },
+  {
+    "revision": "07df4f71ec70df2b5550",
+    "url": "/React-Address-Book/static/js/runtime-main.a216bb5e.js"
+  }
+]);
